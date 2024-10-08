90fd1870932e68322987c3fa3db7ae7b038ba32a
302A-15\cre <a>
2024-10-08 19:24:01 +0900

    detailを編集していく

--- database/migrations/2022_12_12_102505_create_posts_table.php
+++ database/migrations/2022_12_12_102505_create_posts_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->unsignedBigInteger('reply_to')->default(0);
             $table->boolean('is_deleted')->default(0);
             $table->timestamps();
+            $table->unsignedBigInteger('parent_id')->default(0);
         });
     }
 
